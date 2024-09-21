131df7192c1b703e1d4e28325543d797bf7ff1ce
guero-comec <ruggeiro@arle.com.br>
2024-09-21 18:13:00 -0300

    Atualizado em 21-09-2024, por Asgard (casa)_

--- docker/settings.py
+++ docker/settings.py
@@ -83,6 +83,16 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
